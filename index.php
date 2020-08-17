c1ee6f88a00d6447a0d694b624bd5b976d15db79
amnatusick <amnatusick@gmail.com>
2020-08-17 16:51:31 +0300

    cearea index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Lectia 30</title>
+</head>
+<body>
+    <h1>Lectia 30</h1>
+</body>
+</html>
